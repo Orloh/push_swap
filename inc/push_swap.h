24c02afc4ea42b4bbfc5269daf158070e8778c35
Orloh <ohercelli@gmail.com>
2025-12-28 22:51:53 +0100

    build: update push_swap.h with parsing and sorting prototypes

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,13 +6,26 @@
 /*   By: orhernan <ohercelli@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 00:46:34 by orhernan          #+#    #+#             */
-/*   Updated: 2025/12/08 01:19:52 by orhernan         ###   ########.fr       */
+/*   Updated: 2025/12/28 22:44:10 by orhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <unistd.h>
+# include <stdlib.h>
+
+# include "ft_printf.h"
 # include "libft.h"
+
+typedef struct s_node_content
+{
+	int	value;
+	int	rank;
+}	t_stack_node;
+
+int	*ft_sort_array(int *arr, int size);
+int	*ft_parse_args(int argc, char **argv, int *size);
 
 #endif
