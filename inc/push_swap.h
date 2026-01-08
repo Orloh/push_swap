81fbeae29d2b3c2a3794dd46e65823e6abe502fc
Orloh <ohercelli@gmail.com>
2026-01-08 07:08:20 +0100

    refactor: update ft_sort_array to take t_int_arr by pointer

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -31,7 +31,7 @@ typedef struct s_int_arr
 	int	size;
 }	t_int_arr;
 
-void	ft_sort_array(t_int_arr arr);
+void	ft_sort_array(t_int_arr *arr);
 t_int_arr	ft_parse_args(int argc, char **argv);
 void	ft_free_int_arr(t_int_arr *arr);
 t_list	*ft_init_stack(t_int_arr *arr);
--- src/sort.c
+++ src/sort.c
@@ -15,25 +15,25 @@
 /*
  * Sorts an array of integers in place and returns a pointer
  */
-void	ft_sort_array(t_int_arr arr)
+void	ft_sort_array(t_int_arr *arr)
 {
 	int	i;
 	int	j;
 	int	tmp;
 
-	if (!arr.data || arr.size <= 1)
+	if (!arr->data || arr->size <= 1)
 		return ;
 	i = 1;
-	while (i < arr.size)
+	while (i < arr->size)
 	{
-		tmp = arr.data[i];
+		tmp = arr->data[i];
 		j = i - 1;
-		while (j >= 0 && arr.data[j] > tmp)
+		while (j >= 0 && arr->data[j] > tmp)
 		{
-			arr.data[j + 1] = arr.data[j];
+			arr->data[j + 1] = arr->data[j];
 			j--;
 		}
-		arr.data[j + 1] = tmp;
+		arr->data[j + 1] = tmp;
 		i++;
 	}
 	return ;
