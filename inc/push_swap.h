9a35ff6e06837cec3727540bfc7bcd4e59a54b3a
Orloh <ohercelli@gmail.com>
2026-01-11 21:29:17 +0100

    feat: implement rotate operations for stacks A and B

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: orhernan <ohercelli@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 00:46:34 by orhernan          #+#    #+#             */
-/*   Updated: 2026/01/11 16:16:59 by orhernan         ###   ########.fr       */
+/*   Updated: 2026/01/11 21:06:11 by orhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,7 @@ void	pa(t_stacks *stacks);
 void	pb(t_stacks *stacks);
 void	sa(t_stacks *stacks);
 void	sb(t_stacks *stacks);
+void	ra(t_stacks *stacks);
+void	rb(t_stacks *stacks);
 
 #endif
---
+++ src/rotate_op.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_op.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orhernan <ohercelli@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/11 20:32:05 by orhernan          #+#    #+#             */
+/*   Updated: 2026/01/11 21:13:26 by orhernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "push_swap.h"
+
+int	ft_rotate(t_list **stack)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return (0);
+	first = *stack;
+	*stack = first->next;
+	first->next = NULL;
+	last = ft_lstlast(*stack);
+	last->next = first;
+	return (1);
+}
+
+void	ra(t_stacks *stacks)
+{
+	if (ft_rotate(&(stacks->a)))
+		ft_printf("ra\n");
+}
+
+void	rb(t_stacks *stacks)
+{
+	if (ft_rotate(&(stacks->b)))
+		ft_printf("rb\n");
+}
