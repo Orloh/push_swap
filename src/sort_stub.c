3aadf8f881540a737878413dd424ea58aa746031
Orloh <ohercelli@gmail.com>
2026-01-21 20:55:34 +0100

    feat: implement complete sorting logic for small stacks (4-5 elements)

--- src/sort_stub.c
+++ src/sort_stub.c
@@ -11,17 +11,65 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <limits.h>
 
-void	ft_tiny_sort(t_stacks *stacks)
+static int	ft_get_min_idx(t_list *stack)
 {
-	(void)stacks;
-	return ;
+	int	min_rank;
+	int	min_idx;
+	int	curr_idx;
+
+	min_rank = INT_MAX;
+	min_idx = 0;
+	curr_idx = 0;
+	while (stack)
+	{
+		if (((t_node_content *)stack->content)->rank < min_rank)
+		{
+			min_rank = ((t_node_content *)stack->content)->rank;
+			min_idx = curr_idx;
+		}
+		stack = stack->next;
+		curr_idx++;
+	}
+
+	return (min_idx);
+}
+
+static void	ft_push_min(t_stacks *stacks)
+{
+	int	min_idx;
+	int	size;
+
+	size = ft_lstsize(stacks->a);
+	min_idx = ft_get_min_idx(stacks->a);
+	if (min_idx <= size/2)
+	{
+		while (min_idx-- > 0)
+			ra(stacks);
+	}
+	else
+	{
+		while (min_idx++ < size)
+			rra(stacks);
+	}
+	pb(stacks);
 }
 
 void	ft_small_sort(t_stacks *stacks)
 {
-	(void)stacks;
-	return ;
+	int	size;
+
+	if (ft_is_sorted_stack(stacks->a))
+		return ;
+	size = ft_lstsize(stacks->a);
+	if (size == 5)
+		ft_push_min(stacks);
+	if (size >= 4)
+		ft_push_min(stacks);
+	ft_tiny_sort(stacks);
+	while (stacks->b)
+		pa (stacks);
 }
 
 void	ft_big_sort(t_stacks *stacks)
@@ -30,8 +78,5 @@ void	ft_big_sort(t_stacks *stacks)
 	return ;
 
 }
-int	ft_is_sorted_stack(t_list *stack)
-{
-	(void)stack;
-	return (0);
-}
+
+
