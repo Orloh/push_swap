cfd621761ec5830993da1d364336f13bb8dc4180
orhernan <ohercelli@gmail.com>
2026-01-28 18:57:17 +0100

    wip: implementig base 3 radix sort

--- src/sort_big.c
+++ src/sort_big.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "push_swap.h"
 #include <limits.h>
 
@@ -32,7 +33,7 @@ static int	ft_get_max_bits(t_list *stack)
 	return (max_bits);
 }
 
-void	ft_big_sort(t_stacks *stacks)
+void	ft_big_sort_base1(t_stacks *stacks)
 {
 	int	i;
 	int	j;
@@ -57,4 +58,38 @@ void	ft_big_sort(t_stacks *stacks)
 		i++;
 	}
 	return ;
+}
+
+void	ft_big_sort(t_stacks *stacks)
+{
+	int	i;
+	int	j;
+	int	size;
+	int	max_trits;
+	int	pow_3;
+
+	max_trits = 6;
+	pow_3 = 1;
+	for(i = 0; i < max_trits; i++)
+	{
+		size = ft_lstsize(stacks->a);
+		for (j = 0; j < size; j++)
+		{
+			int rank = ((t_node_content *)stacks->a->content)->rank;
+			int digit = (rank / pow_3) % 3;
+			if (digit == 0)
+			{
+				pb (stacks);
+				rb (stacks);
+			}
+			else if (digit == 1)
+				pb(stacks);
+			else
+				ra(stacks);
+		}
+		while (stacks->b)
+			pa(stacks);
+		pow_3 *= 3;
+	}
+
 }
