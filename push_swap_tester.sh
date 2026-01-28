89f91c0aceba2e91174db8159f32e1e7118e545b
orhernan <ohercelli@gmail.com>
2026-01-28 17:45:20 +0100

    feat: added checker feedback to tester and colored result status

--- push_swap_tester.sh
+++ push_swap_tester.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
 # Configuration
 ITERATIONS=$1
 SIZE=$2
 PROGRAM="./push_swap"
+CHECKER="./checker_linux"
 
 if [ "$#" -ne 2 ]; then
 	echo "Usage: ./tester.sh [number_of_runs] [stack_size]"
@@ -18,18 +24,27 @@ echo "Running $ITERATIONS tests with $SIZE numbers..."
 
 for ((i=1; i<=$ITERATIONS; i++))
 do
+
 	#Generate random numbers
 	ARG=$(seq -10000 10000 | shuf -n $SIZE | tr '\n' ' ')
 
-	#RUN push_swap and count lines
+	#Run push_swap and count lines
 	RESULT=$($PROGRAM $ARG | wc -l | tr -d ' ')
 
-	#Update stats
+	# Run the checker and store result
+	CHECK_RESULT=$($PROGRAM $ARG | $CHECKER $ARG)
+
+	if [ "$CHECK_RESULT" = "OK" ]; then
+		PRINT_CHECK="${GREEN}OK${RESET}"
+	else
+		PRINT_CHECK="${RED}KO${RESET}"
+	fi
+
 	TOTAL=$((TOTAL + RESULT))
 	if [ $RESULT -lt $MIN ]; then MIN=$RESULT; fi
 	if [ $RESULT -gt $MAX ]; then MAX=$RESULT; fi
 
-	echo "Run $i: $RESULT instructions"
+	echo -e "Run $i: $RESULT instructions [$PRINT_CHECK]"
 done
 
 AVERAGE=$((TOTAL / ITERATIONS))
