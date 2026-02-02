66fc2a191bcd8efe21fc5b5e82d86911c2561c4c
orhernan <ohercelli@gmail.com>
2026-02-02 19:03:33 +0100

    refactor(main): modularize logic and Norminette compliance

--- src/main.c
+++ src/main.c
@@ -3,20 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orhernan <ohercelli@gmail.com>             +#+  +:+       +#+        */
+/*   By: orhernan <orhernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 15:56:36 by orhernan          #+#    #+#             */
-/*   Updated: 2026/01/27 19:52:30 by orhernan         ###   ########.fr       */
+/*   Updated: 2026/02/02 18:37:25 by orhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <unistd.h>
 
-void	ft_error(void)
+static void	ft_error(void)
 {
 	ft_putstr_fd("Error\n", STDERR_FILENO);
 	exit(1);
+}
+
+static void	ft_sort_dispatcher(t_stacks *stacks, int size)
+{
+	if (size <= 5)
+		ft_small_sort(stacks);
+	else
+		ft_big_sort(stacks);
+}
+
+void	ft_free_all(t_stacks *stacks, t_int_arr *arr)
+{
+	if (stacks)
+	{
+		ft_clear_stacks(stacks);
+		free(stacks);
+	}
+	if (arr)
+		ft_free_int_arr(arr);
 }
 
 int	main(int argc, char **argv)
@@ -35,17 +54,11 @@ int	main(int argc, char **argv)
 	stacks->a = ft_init_stack(&num_arr);
 	if (!stacks->a)
 	{
-		free(stacks);
-		ft_free_int_arr(&num_arr);
+		ft_free_all(stacks, &num_arr);
 		ft_error();
 	}
 	stacks->b = NULL;
-	if (num_arr.size <= 5)
-		ft_small_sort(stacks);
-	else
-		ft_big_sort(stacks);	
-	clear_stacks(stacks);
-	free(stacks);
-	ft_free_int_arr(&num_arr);
+	ft_sort_dispatcher(stacks, num_arr.size);
+	ft_free_all(stacks, &num_arr);
 	return (0);
 }
